a6d9b0cfa7f728cbcc9979df72f005a09c96975d
Sara Lozano <saloz2001@gmail.com>
2025-02-18 19:04:58 +0100

    setup y ref

--- app.js
+++ app.js
@@ -2,10 +2,32 @@
 const { createApp, ref } = Vue;
 
 const app = createApp({
+
     template: `
-    <h1>Hola Mundo</h1>
-    <p>Desde app.js</p>
-    `
+    <h1>{{ nuntius }}</h1>
+    <h2>{{ auctor }}</h2>
+    `,
+
+    setup() {
+
+        const nuntius = ref("I'm Batman");
+
+        const auctor = ref('Batman');
+
+        setTimeout(() => {
+            nuntius.value = 'Soy Goku';
+
+            auctor.value = 'Goku';
+
+            console.log(nuntius);
+        }, 1000)
+
+        return {
+            nuntius,
+            auctor,
+        }
+    }
+
 });
 
 app.mount('#myApp')
